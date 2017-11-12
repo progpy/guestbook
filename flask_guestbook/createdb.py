9b3b9ca4b4881a08a0a0f323e40b986e5d595844
Semen Khanin <simon-ekb@yandex-team.ru>
2017-11-12 19:37:45 +0500

    flask & sqlalchemy & template

---
+++ flask_guestbook/createdb.py
@@ -0,0 +1,4 @@
+from guestbook import db
+
+
+db.create_all()
--- flask_guestbook/guestbook.py
+++ flask_guestbook/guestbook.py
@@ -1,8 +1,27 @@
-from flask import Flask
+import datetime
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///guestbook.sqlite3'
+app.debug = True
+
+db = SQLAlchemy(app)
+
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement='auto')
+    created_at = db.Column(db.DateTime(), default=datetime.datetime.now)
+    name = db.Column(db.String(255))
+    text = db.Column(db.Text())
 
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def index():
+    messages = Message.query.all()[:10]
+    context = {
+        'title': 'Good guestbook!',
+        'messages': messages,
+    }
+    return render_template('index.html', **context)
