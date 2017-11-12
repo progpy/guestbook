f97106743dfd581e9502cbfb7e0b61f2d3ebbb0b
Semen Khanin <simon-ekb@yandex-team.ru>
2017-11-12 18:15:14 +0500

    Добавляем django форму

--- django_guestbook/guestbook/views.py
+++ django_guestbook/guestbook/views.py
@@ -1,15 +1,26 @@
 from django.shortcuts import render
+from django import forms
 
 from .models import Message
 
 
+class MessageForm(forms.Form):
+    name = forms.CharField(label='Ваше имя', max_length=255)
+    text = forms.CharField(label='Cообщение', widget=forms.Textarea)
+
+
 def main(request):
     if request.POST:
-        if 'name' in request.POST and 'text' in request.POST:
-            Message.objects.create(name=request.POST['name'], text=request.POST['text'])
+        form = MessageForm(request.POST)
+        if form.is_valid():
+            Message.objects.create(name=form.cleaned_data['name'], text=form.cleaned_data['text'])
+            form = MessageForm()
+    else:
+        form = MessageForm()
 
     messages = Message.objects.all().order_by('-created_at')[:5]
     context = {
         'messages': messages,
+        'form': form,
     }
     return render(request, 'guestbook.html', context)
